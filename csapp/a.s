3a122b66be6074027ba176267c92af323ded1a0b
zhangcl <zhangch@heptax.com>
2020-06-07 22:24:45 +0800

    compiled in Linux

--- csapp/a.s
+++ csapp/a.s
@@ -1,94 +1,77 @@
-	.section	__TEXT,__text,regular,pure_instructions
-	.build_version macos, 10, 14
-	.globl	_main                   ## -- Begin function main
-	.p2align	4, 0x90
-_main:                                  ## @main
+	.file	"Sum.c"
+	.section	.rodata
+.LC0:
+	.string	"The result is %d.\n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
 	.cfi_startproc
-## %bb.0:
 	pushq	%rbp
 	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
+	.cfi_offset 6, -16
 	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	subq	$32, %rsp
-	leaq	-16(%rbp), %rdi
-	movq	___stack_chk_guard@GOTPCREL(%rip), %rax
-	movq	(%rax), %rax
-	movq	%rax, -8(%rbp)
-	movl	$2, -20(%rbp)
-	movq	L_main.arr(%rip), %rax
-	movq	%rax, -16(%rbp)
-	movl	-20(%rbp), %esi
-	callq	_rSum
-	leaq	L_.str(%rip), %rdi
-	movl	%eax, -24(%rbp)
-	movl	-24(%rbp), %esi
-	movb	$0, %al
-	callq	_printf
-	movq	___stack_chk_guard@GOTPCREL(%rip), %rdi
-	movq	(%rdi), %rdi
-	movq	-8(%rbp), %rcx
-	cmpq	%rcx, %rdi
-	movl	%eax, -28(%rbp)         ## 4-byte Spill
-	jne	LBB0_2
-## %bb.1:
-	xorl	%eax, %eax
-	addq	$32, %rsp
-	popq	%rbp
-	retq
-LBB0_2:
-	callq	___stack_chk_fail
-	ud2
-	.cfi_endproc
-                                        ## -- End function
-	.globl	_rSum                   ## -- Begin function rSum
-	.p2align	4, 0x90
-_rSum:                                  ## @rSum
-	.cfi_startproc
-## %bb.0:
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset %rbp, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register %rbp
-	subq	$32, %rsp
-	movq	%rdi, -16(%rbp)
-	movl	%esi, -20(%rbp)
-	cmpl	$0, -20(%rbp)
-	jg	LBB1_2
-## %bb.1:
-	movl	$0, -4(%rbp)
-	jmp	LBB1_3
-LBB1_2:
-	movq	-16(%rbp), %rax
-	movl	(%rax), %ecx
-	movq	-16(%rbp), %rax
-	addq	$4, %rax
-	movl	-20(%rbp), %edx
-	subl	$1, %edx
-	movq	%rax, %rdi
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	$2, -4(%rbp)
+	movl	$1, -16(%rbp)
+	movl	$2, -12(%rbp)
+	movl	-4(%rbp), %edx
+	leaq	-16(%rbp), %rax
 	movl	%edx, %esi
-	movl	%ecx, -24(%rbp)         ## 4-byte Spill
-	callq	_rSum
-	movl	-24(%rbp), %ecx         ## 4-byte Reload
-	addl	%eax, %ecx
-	movl	%ecx, -4(%rbp)
-LBB1_3:
-	movl	-4(%rbp), %eax
-	addq	$32, %rsp
-	popq	%rbp
-	retq
+	movq	%rax, %rdi
+	call	rSum
+	movl	%eax, -8(%rbp)
+	movl	-8(%rbp), %eax
+	movl	%eax, %esi
+	movl	$.LC0, %edi
+	movl	$0, %eax
+	call	printf
+	leave
+	.cfi_def_cfa 7, 8
+	ret
 	.cfi_endproc
-                                        ## -- End function
-	.section	__TEXT,__literal8,8byte_literals
-	.p2align	2               ## @main.arr
-L_main.arr:
-	.long	1                       ## 0x1
-	.long	2                       ## 0x2
-
-	.section	__TEXT,__cstring,cstring_literals
-L_.str:                                 ## @.str
-	.asciz	"The result is %d.\n"
-
-
-.subsections_via_symbols
+.LFE0:
+	.size	main, .-main
+	.globl	rSum
+	.type	rSum, @function
+rSum:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	pushq	%rbx
+	subq	$24, %rsp
+	.cfi_offset 3, -24
+	movq	%rdi, -24(%rbp)
+	movl	%esi, -28(%rbp)
+	cmpl	$0, -28(%rbp)
+	jg	.L3
+	movl	$0, %eax
+	jmp	.L4
+.L3:
+	movq	-24(%rbp), %rax
+	movl	(%rax), %ebx
+	movl	-28(%rbp), %eax
+	leal	-1(%rax), %edx
+	movq	-24(%rbp), %rax
+	addq	$4, %rax
+	movl	%edx, %esi
+	movq	%rax, %rdi
+	call	rSum
+	addl	%ebx, %eax
+.L4:
+	addq	$24, %rsp
+	popq	%rbx
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	rSum, .-rSum
+	.ident	"GCC: (GNU) 4.8.5 20150623 (Red Hat 4.8.5-39)"
+	.section	.note.GNU-stack,"",@progbits
